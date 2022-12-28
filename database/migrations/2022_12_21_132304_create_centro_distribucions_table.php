b64a61e9eac8bd4de6521706a19e6fe25aad2409
Felipeopazoc <felipeopazo2409@gmail.com>
2022-12-27 22:11:25 -0300

    first commit

---
+++ database/migrations/2022_12_21_132304_create_centro_distribucions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('centro_distribucions', function (Blueprint $table) {
+            $table->id();
+            $table->integer("cd_codigo");
+            $table->integer("cd_direccion");
+            $table->integer("cd_telefono");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('centro_distribucions');
+    }
+};
